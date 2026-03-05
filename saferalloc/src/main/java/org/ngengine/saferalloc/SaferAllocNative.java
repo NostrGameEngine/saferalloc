e08ffd114329fac545b32a14b29bc53ba96d8366
Riccardo Balbo <os@rblb.it>
2026-03-05 15:37:48 +0100

    expose SaferAllocNative

--- saferalloc/src/main/java/org/ngengine/saferalloc/SaferAllocNative.java
+++ saferalloc/src/main/java/org/ngengine/saferalloc/SaferAllocNative.java
@@ -3,23 +3,23 @@ package org.ngengine.saferalloc;
 import java.nio.ByteBuffer;
 
 
-final class SaferAllocNative {
+public final class SaferAllocNative {
   private SaferAllocNative() {}
 
-  static native long malloc(long size);
-  static native long calloc(long count, long size);
-  static native long realloc(long addr, long newSize);
-  static native void free(long addr);
-  static native long mallocAligned(long size, long alignment);
-  static native long currentAllocatedBytes();
-  static native long mallocFunctionPointer();
-  static native long callocFunctionPointer();
-  static native long reallocFunctionPointer();
-  static native long freeFunctionPointer();
-  static native long alignedAllocFunctionPointer();
-  static native long alignedFreeFunctionPointer();
-  static native int pointerSizeBytes();
+  public static native long malloc(long size);
+  public static native long calloc(long count, long size);
+  public static native long realloc(long addr, long newSize);
+  public static native void free(long addr);
+  public static native long mallocAligned(long size, long alignment);
+  public static  native long currentAllocatedBytes();
+  public static native long mallocFunctionPointer();
+  public static native long callocFunctionPointer();
+  public static native long reallocFunctionPointer();
+  public static native long freeFunctionPointer();
+  public static native long alignedAllocFunctionPointer();
+  public static native long alignedFreeFunctionPointer();
+  public static native int pointerSizeBytes();
 
-  static native ByteBuffer wrapMemByteBuffer(long addr, long size);
-  static native long addressMemByteBuffer(ByteBuffer buffer);
+  public static native ByteBuffer wrapMemByteBuffer(long addr, long size);
+  public static native long addressMemByteBuffer(ByteBuffer buffer);
 }
